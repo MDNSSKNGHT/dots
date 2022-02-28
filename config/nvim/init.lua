d6c850efba9d835f746f2d3ed78a88a60d3d17eb
Leonardo Estacio <mdnssknght@gmail.com>
2022-02-28 14:27:40 -0500

    Initial commit

---
+++ config/nvim/init.lua
@@ -0,0 +1,37 @@
+local Plug = vim.fn['plug#']
+
+-- What about plugins?
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug('scrooloose/nerdtree')
+
+Plug('neoclide/coc.nvim', {branch = 'release'})
+
+Plug('chriskempson/base16-vim')
+
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+
+-- Initialize plugin system
+vim.call('plug#end')
+
+-- Columns
+vim.opt.number = true
+
+-- Theme
+vim.opt.termguicolors = true
+vim.cmd('colorscheme base16-classic-dark')
+
+-- Treesitter
+require'nvim-treesitter.configs'.setup {
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = true
+	}
+}
+
+-- Tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
